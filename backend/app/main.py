6f963276beb20a6a21bc13878faace91a2cd628c
sud-hub <sudu.khot@gmail.com>
2026-06-22 23:06:58 +0530

    fix: Add port number in main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -163,3 +163,9 @@ def create_app(*, testing: bool = False) -> FastAPI:
 
 
 app = create_app()
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
